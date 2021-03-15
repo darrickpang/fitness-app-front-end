85109c17e7cdc12973644bc7c1e3c3f0e861de24
darrickpang <dkypang@yahoo.com>
2021-03-14 17:50:16 -0700

    kudos

--- src/containers/UserMainContent.js
+++ src/containers/UserMainContent.js
@@ -38,7 +38,7 @@ class UserMainContent extends React.Component {
             <div>
                 {this.state.activities.map(activity => {
                     return(
-                        <p>{activity.name}, {activity.start_date}</p>
+                        <p>{activity.name}, {activity.start_date_local}, {activity.type}, {activity.kudos_count}</p>
                     )
                 })}
             </div>
